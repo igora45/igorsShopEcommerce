1cc6571011442cd68d1ad8677e5f02cfdb51a6ff
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2023-04-26 18:14:45 -0300

    style: underline in "our products" title

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -18,14 +18,23 @@ const Title = styled.p`
   font-size: 40px;
   text-align: center;
   margin: 0 1rem;
-  margin-bottom: 3rem;
+
   overflow-x: hidden;
+`;
+const UnderLine = styled.div`
+  width: 8rem;
+  height: 0.3rem;
+  background-color: #d1e5f8;
+  border-radius: 0.5rem;
+  margin: 10px auto;
+  margin-bottom: 3rem;
 `;
 
 export const Products = () => {
   return (
     <Container>
       <Title>OUR PRODUCTS</Title>
+      <UnderLine />
       <Wrapper>
         {AllProducts.map(item => (
           <Product item={item} key={item.id} />
